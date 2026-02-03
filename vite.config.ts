2f9f500d0ac9303d84c0578ee4ff0ef797fe1288
Luis Fernando Cordova <luisfernandocordova.24@gmail.com>
2026-02-02 23:18:34 -0700

    changes for endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
+import fs from 'fs';
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 const PORT = 3040;
+const HTTPS_KEY = process.env.VITE_HTTPS_KEY;
+const HTTPS_CERT = process.env.VITE_HTTPS_CERT;
+const HTTPS_CONFIG =
+  HTTPS_KEY && HTTPS_CERT
+    ? {
+        key: fs.readFileSync(HTTPS_KEY),
+        cert: fs.readFileSync(HTTPS_CERT),
+      }
+    : undefined;
 
 export default defineConfig({
   plugins: [react()],
@@ -18,7 +28,7 @@ export default defineConfig({
       },
     ],
   },
-  server: { port: PORT, host: true },
+  server: { port: PORT, host: true, https: HTTPS_CONFIG },
   preview: { port: PORT, host: true },
 });
 
